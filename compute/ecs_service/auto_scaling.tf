52bcd0a63c5c00e41739cc4b3336e555fa1e61a1
Mina Abadir <mina@abadir.email>
2026-04-14 21:52:10 -0400

    autoscaling ignore changes

--- compute/ecs_service/auto_scaling.tf
+++ compute/ecs_service/auto_scaling.tf
@@ -12,6 +12,10 @@ resource "aws_appautoscaling_target" "this" {
   service_namespace  = "ecs"
 
   tags = local.tags
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 ################################################################################
