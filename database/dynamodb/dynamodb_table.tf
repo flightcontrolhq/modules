093461f8a19bb80d36772b7f4be0a6f95f5506cd
Mina Abadir <mina@abadir.email>
2026-04-16 23:05:18 -0400

    fixing ddb deprecation warning

--- database/dynamodb/dynamodb_table.tf
+++ database/dynamodb/dynamodb_table.tf
@@ -29,12 +29,23 @@ resource "aws_dynamodb_table" "this" {
     for_each = var.global_secondary_indexes
     content {
       name               = global_secondary_index.value.name
-      hash_key           = global_secondary_index.value.hash_key
-      range_key          = global_secondary_index.value.range_key
       projection_type    = global_secondary_index.value.projection_type
       non_key_attributes = global_secondary_index.value.non_key_attributes
       read_capacity      = local.is_provisioned ? global_secondary_index.value.read_capacity : null
       write_capacity     = local.is_provisioned ? global_secondary_index.value.write_capacity : null
+
+      key_schema {
+        attribute_name = global_secondary_index.value.hash_key
+        key_type       = "HASH"
+      }
+
+      dynamic "key_schema" {
+        for_each = global_secondary_index.value.range_key != null ? [global_secondary_index.value.range_key] : []
+        content {
+          attribute_name = key_schema.value
+          key_type       = "RANGE"
+        }
+      }
     }
   }
 
